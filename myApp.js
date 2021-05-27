fc076c4c50c50d093b63068a1cdb00823f54acb5
Koji Okajima <koji970124@gmail.com>
2021-05-26 17:29:15 -0700

    q4

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+var absolutePath = __dirname + "/views/index.html"
+console.log(absolutePath);
 console.log("Hello World");
 
 
 app.get('/', (req, res, next) => {
   // res.send("Hello Express")
-  res.sendFile("/views/index.html")
+  res.sendFile(absolutePath)
 })
 
 
